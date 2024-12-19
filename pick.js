5efe91bc2defc7c4fa24f4238417c7c400733e73
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-19 23:18:45 +0530

    Update pick.js

--- pick.js
+++ pick.js
@@ -1,12 +1,16 @@
- // Picks specific properties from an object
+/**
+ * Picks specific properties from an object.
+ * @param {Object} object - The source object.
+ * @param {Array} properties - The list of properties to pick.
+ * @returns {Object} - The new object with only the picked properties.
+ */
 const pick = (object, properties) => {
-  let picked = {};
-  for (let key in object || (object = {})) {
-    if (Object.hasOwnProperty.call(object, key) && properties.includes(key)) {
-      picked[key] = object[key];
+  return Object.entries(object || {}).reduce((picked, [key, value]) => {
+    if (properties.includes(key)) {
+      picked[key] = value;
     }
-  }
-  return picked;
+    return picked;
+  }, {});
 };
 
 export default pick;
